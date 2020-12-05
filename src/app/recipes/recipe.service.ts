b784fff362b55bb526db102e0eee24b3e4b45d9c
AURORA_NT\yewtuck.cheah <yewtuck.cheah@rovisys.com>
2020-12-05 17:57:52 +0800

    send selectedRecipe from recipe-item to recipes component

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,8 @@
+import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
     new Recipe('Test Recipe', 'this is a simple test', 'https://www.cookingclassy.com/wp-content/uploads/2019/05/fiesta-rice-recipe-7.jpg'),
     new Recipe('Another Test Recipe', 'this is another simple test', 'https://www.cookingclassy.com/wp-content/uploads/2019/05/fiesta-rice-recipe-7.jpg'),
