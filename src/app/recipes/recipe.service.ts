583fe1ce2ca3878ab0a9ee3c598f53c46d39c52e
AURORA_NT\yewtuck.cheah <yewtuck.cheah@rovisys.com>
2020-12-05 23:27:27 +0800

    add ingredients to shopping list from recipe-detail using services

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,44 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Recipe } from './recipe.model';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'this is a simple test', 'https://www.cookingclassy.com/wp-content/uploads/2019/05/fiesta-rice-recipe-7.jpg'),
-    new Recipe('Another Test Recipe', 'this is another simple test', 'https://www.cookingclassy.com/wp-content/uploads/2019/05/fiesta-rice-recipe-7.jpg'),
-    new Recipe('Third Test Recipe', 'this is a third test', 'https://www.cookingclassy.com/wp-content/uploads/2019/05/fiesta-rice-recipe-7.jpg')
+    new Recipe(
+      'Test Recipe',
+      'this is a simple test',
+      'https://www.cookingclassy.com/wp-content/uploads/2019/05/fiesta-rice-recipe-7.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French fries', 20)
+      ]),
+    new Recipe(
+      'Another Test Recipe',
+      'this is another simple test',
+      'https://www.cookingclassy.com/wp-content/uploads/2019/05/fiesta-rice-recipe-7.jpg',
+      [
+        new Ingredient('Cabbage', 1),
+        new Ingredient('Buns', 2)
+      ]),
+    new Recipe(
+      'Third Test Recipe',
+      'this is a third test',
+      'https://www.cookingclassy.com/wp-content/uploads/2019/05/fiesta-rice-recipe-7.jpg',
+      [
+        new Ingredient('Carrot', 1)
+      ])
   ];
+
+  constructor(private shoppingListService: ShoppingListService) {}
 
   getRecipes(): Recipe[] {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]): void {
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
