8c0affc54b5d26239102717a880d96a50d5a21a8
AURORA_NT\yewtuck.cheah <yewtuck.cheah@rovisys.com>
2020-12-15 14:08:46 +0800

    link recipe-edit form to reactive form

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,13 +11,38 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false;
+  recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private recipeService: RecipeService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
       this.id = +params.id;
       this.editMode = params.id != null;
+      this.initForm();
     });
+  }
+
+  private initForm(): void {
+    let recipeName = '';
+    let recipeDescription = '';
+    let recipeImagePath = '';
+
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipe(this.id);
+      recipeName = recipe.name;
+      recipeDescription = recipe.description;
+      recipeImagePath = recipe.imagePath;
+    }
+    this.recipeForm = new FormGroup({
+      name: new FormControl(recipeName),
+      imagePath: new FormControl(recipeImagePath),
+      description: new FormControl(recipeDescription),
+    });
+  }
+
+  onSubmit(): void {
+
   }
 }
