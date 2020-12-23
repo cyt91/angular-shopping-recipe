929100db2a64f1c57708b8f370b7b6f551faa943
AURORA_NT\yewtuck.cheah <yewtuck.cheah@rovisys.com>
2020-12-23 17:51:25 +0800

    navigate to recipes page when authenticated

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthResponseData, AuthService } from './auth.service';
 
@@ -13,7 +14,7 @@ export class AuthComponent {
   isLoading = false;
   error: string = null;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   onSwitchMode(): void {
     this.isLoginMode = !this.isLoginMode;
@@ -30,6 +31,7 @@ export class AuthComponent {
     authObs.subscribe(responseData => {
       console.log(responseData);
       this.isLoading = false;
+      this.router.navigate(['./recipes']);
     }, errorMsg => {
       console.error(errorMsg);
       this.error = errorMsg;
