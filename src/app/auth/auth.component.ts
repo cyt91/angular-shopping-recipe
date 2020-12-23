af1a655ed6b16a2543b6f35dff09db42e9e02f3f
AURORA_NT\yewtuck.cheah <yewtuck.cheah@rovisys.com>
2020-12-23 15:42:21 +0800

    add loading spinner and error message to auth page

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -8,13 +9,34 @@ import { NgForm } from '@angular/forms';
 })
 export class AuthComponent {
   isLoginMode = true;
+  isLoading = false;
+  error: string = null;
+
+  constructor(private authService: AuthService) {}
 
   onSwitchMode(): void {
     this.isLoginMode = !this.isLoginMode;
   }
 
   onSubmit(form: NgForm): void {
-    console.log(form.value);
+    if (!form.valid) { return; }
+    const { email, password } = form.value;
+    this.isLoading = true;
+    if (this.isLoginMode) {
+
+    } else {
+      this.authService
+        .signUp(email, password)
+        .subscribe(response => {
+          console.log(response);
+          this.isLoading = false;
+        }, error => {
+          console.error(error);
+          this.error = 'An error occured';
+          this.isLoading = false;
+        });
+    }
+
     form.reset();
   }
 
