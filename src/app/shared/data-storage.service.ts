8d769640f49713296aa53a18bdaf645f87a09b4a
AURORA_NT\yewtuck.cheah <yewtuck.cheah@rovisys.com>
2020-12-24 10:04:47 +0800

    add http interceptor to inject token to validate http requests

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -28,25 +28,18 @@ export class DataStorageService {
   }
 
   fetchRecipes(): Observable<Recipe[]> {
-    return this.authService.user.pipe(
-      take(1),
-      exhaustMap(user => {
-        return this.http
-        .get<Recipe[]>(
-          'https://ng-course-recipe-book-6dd56-default-rtdb.firebaseio.com/recipes.json',
-          {
-            params: new HttpParams().set('auth', user.token)
-          }
-        );
-      }),
-      map(recipes => {
-        return recipes?.map(recipe => {
-          return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] };
-        }) ?? [];
-      }),
-      tap(recipes => {
-        this.recipeService.setRecipes(recipes);
-      })
-    );
+    return this.http
+      .get<Recipe[]>(
+        'https://ng-course-recipe-book-6dd56-default-rtdb.firebaseio.com/recipes.json',
+      ).pipe(
+        map(recipes => {
+          return recipes?.map(recipe => {
+            return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] };
+          }) ?? [];
+        }),
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes);
+        })
+      );
   }
 }
