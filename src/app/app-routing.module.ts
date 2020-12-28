2ca11e4ec8390373ec4fac3d6695d1ecda212be7
AURORA_NT\yewtuck.cheah <yewtuck.cheah@rovisys.com>
2020-12-28 15:48:54 +0800

    remove unused import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthComponent } from './auth/auth.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
